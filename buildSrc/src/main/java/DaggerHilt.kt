4dc5ae82cf14f04614cca173927d4c55c54f4b81
ashutosh57 <ashutosh@binaryveda.com>
2023-09-04 20:10:16 +0530

    inital project with hilt implemention

---
+++ buildSrc/src/main/java/DaggerHilt.kt
@@ -0,0 +1,6 @@
+object DaggerHilt {
+    const val androidVersion = "2.44.2"
+    const val compilerVersion = "2.44"
+    const val hiltAndroid = "com.google.dagger:hilt-android:$androidVersion"
+    const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$compilerVersion"
+}
---
+++ presentation/src/main/java/com/ashutoshwahane/myapplication/ModernApplication.kt
@@ -0,0 +1,8 @@
+package com.ashutoshwahane.myapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ModernApplication: Application() {
+}
